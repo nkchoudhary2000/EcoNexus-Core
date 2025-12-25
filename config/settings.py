610b1ad404c00191902b5ba25fe5af726b6996f2
Niraj Choudhary <niraj.choudhary1995@gmail.com>
2025-12-25 09:06:45 +0530

    feat: Add initial Django project settings and update Vercel static build directory.

--- config/settings.py
+++ config/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
